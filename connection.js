d492ab66ac7f928a4c466a1d2ae1264b6836ebb3
mraniket8975 <128958775+mraniket8975@users.noreply.github.com>
2023-03-27 13:09:04 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-let url = process.env.MONGO_URL
+
 let params = {
     useNewUrlParser : true,
     useUnifiedTopology : true
 }
-mongoose.connect(url , params)
+mongoose.connect('mongodb+srv://aniket123:dyL0u7eGwuthc6J8@cluster0.7gys0dg.mongodb.net/meet?retryWrites=true&w=majority' , params)
 
 let db  = mongoose.connection;
 
